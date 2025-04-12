5aa9d212a146c47326a411b5c9a53ee0821834ed
Kristian Trenskow <this.is@kristians.email>
2025-04-12 12:18:04 +0200

    iOS 18.

--- Package.swift
+++ Package.swift
@@ -4,21 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "AsyncBroadcast",
+	name: "AsyncBroadcast",
 	platforms: [
-		.iOS(.v16),
+		.iOS(.v18),
 		.macOS(.v15)
 	],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "AsyncBroadcast",
-            targets: ["AsyncBroadcast"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "AsyncBroadcast"),
-    ]
+	products: [
+		// Products define the executables and libraries a package produces, making them visible to other packages.
+		.library(
+			name: "AsyncBroadcast",
+			targets: ["AsyncBroadcast"]),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package, defining a module or a test suite.
+		// Targets can depend on other targets in this package and products from dependencies.
+		.target(
+			name: "AsyncBroadcast"),
+	]
 )
+
